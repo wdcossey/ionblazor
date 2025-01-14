9df46e7f43260ccf14bcecc9fb76cd55bab11f4f
wdcossey <wdcossey@github.com>
2025-01-14 23:17:36 +0000

    Add sample barebones project for IonBlazor
    Introduce a new sample project "IonBlazor.Barebones" with platform-specific configurations (Windows, Android, and MacCatalyst) and basic components.

--- src/IonBlazor/Extensions/ServiceCollectionExtensions.cs
+++ src/IonBlazor/Extensions/ServiceCollectionExtensions.cs
@@ -3,16 +3,8 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddIonBlazor(this IServiceCollection services)
+    /*public static IServiceCollection AddIonBlazor(this IServiceCollection services)
     {
-/*#if NET8_0_OR_GREATER
-        services.AddKeyedScoped<Task<IJSObjectReference>>(nameof(IonAccordionGroup), async (provider, key) =>
-        {
-            IJSRuntime jsRuntime = provider.GetRequiredService<IJSRuntime>();
-            return await jsRuntime.ImportAsync((string)key!);
-        });
-#endif*/
-
         return services;
-    }
+    }*/
 }
