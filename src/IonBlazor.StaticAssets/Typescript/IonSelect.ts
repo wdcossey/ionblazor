7241731f9c5b48b943366675bf73bc9bd7326c4f
wdcossey <wdcossey@github.com>
2026-05-13 16:21:18 +0100

    - Corrected `IonSelect.ts` merge. - Adjust IonList in CardSample to apply `no-item-padding-start` class.

--- src/IonBlazor.StaticAssets/Typescript/IonSelect.ts
+++ src/IonBlazor.StaticAssets/Typescript/IonSelect.ts
@@ -1,9 +1,5 @@
-<<<<<<<< HEAD:src/IonBlazor.StaticAssets/wwwroot/IonSelect.js
-export function open(element, target) {
-========
 
 export function open(element: HTMLIonSelectElement, target?: HTMLElement): void {
->>>>>>>> claude/update-unit-tests-G9BRc:src/IonBlazor.StaticAssets/Typescript/IonSelect.ts
     if (target) {
         const event = new MouseEvent('click', {
             bubbles: true,
@@ -11,9 +7,12 @@ export function open(element: HTMLIonSelectElement, target?: HTMLElement): void 
             view: window,
             relatedTarget: target,
         });
+
         element.open(event);
+
         return;
     }
+
     element.open();
 }
 
